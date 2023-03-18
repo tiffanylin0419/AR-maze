40f9d015d2190f26526f503ea2982027e1e86fc7
Tiffany <tl330@duke.edu>
2023-03-18 03:01:33 -0400

    upload

---
+++ Assets/ball_script.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ball_script : MonoBehaviour
+{
+    public GameObject plane;
+    public GameObject spawnPoint;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.position.y<plane.transform.position.y-10){
+            transform.position=spawnPoint.transform.position;
+        }
+    }
+}
